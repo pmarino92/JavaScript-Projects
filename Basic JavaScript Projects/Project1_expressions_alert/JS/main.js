e9c278739a1a408812214a7e546dd1b602fc2742
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-07-13 09:47:33 -0700

    Basic JavaScript Projects

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+window.alert('Hello, World'); // User receives alert upon opening html
+
+let myPet = 'river otter'; // Assigning river otter to variable myPet
+let myOtherPet = 'seahorse'; // Assigning seahorse to variable myOtherPet
+document.write('My favorite animal is the ' + myPet + ', and my other favorite animal is a' + myOtherPet);
+
+var Weightlifting = "Exercises", Chest = "Bench Press", Legs = "Squats", Shoulders = "Dumbbell press", Abs = "Crunches"; // Assigned multiple variables
+var Weightlifting = Weightlifting.fontcolor("red"); // Weightlifting variable will be in red font
+document.write(Weightlifting);
+
+var A = 3 + 3 // Defining a variable "A" with a 3+3 expression
+document.write(A) // Outputting to html file the variable A
