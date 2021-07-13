b619b55de6c5244c7f976466d9a72ba2483c2bcc
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-07-13 13:12:54 -0700

    Project 2 Functions

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() { // Creating a function to display a sentence
+    var sentence = "I am learning"; // assigning variable "sentence"
+    sentence += "a lot from this book!";// Concatenating with variable "sentence"
+    document.getElementById("demo").innerHTML = sentence; // retrieving full sentence once function is done
+}
